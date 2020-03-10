2b68a3f3462a22f7acdf099f2d0cfea53af6d9e4
Hannah Thorpe <hthorpe@protonmail.com>
2020-03-10 15:17:19 -0400

    route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
+import { Route } from 'react-router-dom';
+import Main from './Main';
 import NoteRoute from "./NoteRoute";
 import FolderRoute from "./FolderRoute";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    state = {
       notes: [],
       folders: []
     };
-  }
+  
 
   render() {
     return (
       <div className='App'>
+        <Route exact path ='/' component= {NoteRoute} />
         <header className='App-header'>
           <h1>Noteful</h1>
         </header>
-        <NoteRoute notes={this.props.store.notes} />
-        <FolderRoute folders={this.props.store.folders} />
+        {/* <Route path='/NoteRoute' componen */}
+        {/* <NoteRoute notes={this.props.store.notes} />
+        <FolderRoute folders={this.props.store.folders} /> */}
       </div>
     );
   }
